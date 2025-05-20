864dc4a90ef7984c85008414f2c0d4df81391cfe
Sebastien Boutin <seb.boutin44@gmail.com>
2025-05-19 21:41:27 -0600

    Create simple_greeter.rb

---
+++ simple_greeter.rb
@@ -0,0 +1,11 @@
+# simple_greeter.rb
+
+# Ask for user input
+print "What's your name? "
+name = gets.chomp
+
+# Greet the user
+puts "Hello, #{name}!"
+
+# Count characters
+puts "Your name has #{name.length} characters."
